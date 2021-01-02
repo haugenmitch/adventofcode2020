a1d2c4e1bd9c085aba906ad9228f687da8f620a9
Mitchell Haugen <haugenmitch@gmail.com>
2021-01-02 10:12:39 -0500

    add commandline arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+extern crate clap;
+use clap::{App, Arg};
 use std::fs::File;
 use std::io::{BufRead, BufReader, Error, ErrorKind, Read};
 
 fn main() -> Result<(), Error> {
+    let _d = parse_args();
+
     let mut vec = read(File::open("./inputs/01.txt")?)?;
     vec.sort();
 
@@ -36,6 +40,30 @@ fn main() -> Result<(), Error> {
     println!("{} x {} x {} = {}", n3, n4, n5, n3 * n4 * n5);
 
     Ok(())
+}
+
+fn parse_args() -> clap::ArgMatches<'static> {
+    let matches = App::new("Advent of Code 2020")
+        .version("0.1.1")
+        .author("Mitchell Haugen <haugenmitch@gmail.com>")
+        .about("Solutions to the Advent of Code 2020 coding challenge in Rust")
+        .arg(
+            Arg::with_name("day")
+                .short("d")
+                .long("day")
+                .takes_value(true)
+                .help("Day of the challenge, a value from 1-25"),
+        )
+        .arg(
+            Arg::with_name("part")
+                .short("p")
+                .long("part")
+                .takes_value(true)
+                .help("Part of the daily challenge, either 1 or 2"),
+        )
+        .get_matches();
+
+    return matches;
 }
 
 fn read<R: Read>(io: R) -> Result<Vec<i64>, Error> {
